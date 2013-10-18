2b45e18e08f71ce6aa035fba351ac507f14a5d1a
Karl-Aksel Puulmann <macobo@ut.ee>
2013-10-18 21:20:53 +0300

    Spectrum into a folder

---
+++ js/directives/folder.js
@@ -0,0 +1,23 @@
+angular.module('audioVizApp')
+  .directive('folder', function () {
+    var linker = function(scope, element, attrs) {
+      scope.title = attrs.title;
+      scope.open = true;
+      console.log(attrs);
+    };
+
+    return {
+      template: '<div class="folder button">' +
+                  '<div class="bg">' +
+                    '<ul>' +
+                      '<li class="title" ng-class="{open:open}" ng-click="open=!open">{{title}}</li>' +
+                      '<li class="content" ng-transclude ng-show="open"></li>' +
+                    '</ul>' +
+                  '</div></div>',
+      restrict: 'E',
+      transclude: true,
+      scope: { open: "=?" },
+      replace: true,
+      link: linker
+    };
+  });
